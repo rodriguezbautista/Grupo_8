062a0a9623bc37f110473f179a34733b44a1cdc7
Bautista Rodriguez <bautistarodriguez2431@gmail.com>
2024-06-02 16:54:42 -0300

    Update PedidoRechazadoException.java

--- src/excepciones/PedidoRechazadoException.java
+++ src/excepciones/PedidoRechazadoException.java
@@ -1,5 +1,11 @@
 package excepciones;
 
 public class PedidoRechazadoException extends Exception {
-
+	public PedidoRechazadoException() {
+		super("El pedido es invalido");
+	}
+	
+	public PedidoRechazadoException(String message) {
+		super(message);
+	}
 }
